ef5f6026ded4956664cc5abcc85ff33582d0b2f0
Matt Helm <mthelm85@gmail.com>
2024-09-09 10:12:40 -0700

    fix frontend url

--- proxy-server.js
+++ proxy-server.js
@@ -1,22 +1,23 @@
 const express = require('express');
 const axios = require('axios');
-const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
-const port = process.env.PROXY_PORT || 3001; // Use Heroku's provided port or 3001 as fallback
+const port = process.env.PROXY_PORT || 3001;
 
-app.use(bodyParser.json());
-app.use(cors()); // Enable CORS for all routes
+// Use cors middleware
+app.use(cors({
+  origin: process.env.FRONTEND_URL || 'http://localhost:3000'
+}));
+
+app.use(express.json());
 
 app.post('/api/publicAPI/v2/timeseries/data', async (req, res) => {
   try {
-    const response = await axios.post('https://api.bls.gov/publicAPI/v2/timeseries/data/', req.body, {
-      headers: { 'Content-Type': 'application/json' }
-    });
-    res.status(response.status).json(response.data);
+    const response = await axios.post('https://api.bls.gov/publicAPI/v2/timeseries/data/', req.body);
+    res.json(response.data);
   } catch (error) {
-    res.status(error.response ? error.response.status : 500).json(error.response ? error.response.data : { error: 'Internal Server Error' });
+    res.status(500).json({ error: 'An error occurred' });
   }
 });
 
